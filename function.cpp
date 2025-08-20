02a50f2a067f6701f44441cfdb26e09305eb81d1
sunil925business@gmail.com <sunil925business@gmail.com>
2025-08-20 16:04:19 +0530

    recursion

--- function.cpp
+++ function.cpp
@@ -1,22 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // pass by value
-void doSomething (int &num){
-    cout  << num << endl;
-    num +=5;
-    cout  << num << endl;
-    num +=5;
-    cout  << num << endl;
+// void f(int i, int n){
+//     if(i > n){
+//         return;
+//     }
+//     cout << "sunil";
+//     f(i+1, n);
 
+// int sum (int n){
+//     if(n ==0) return 0;
+//     return n + sum(n-1);
+// }
+
+// int fact(int n)
+// {
+
+//     if (n == 0){
+//         return 1;
+//     }
+//     return n * fact(n - 1);
+// }
+// }
+
+// bool f(int i, string &s){
+//     if(i >= s.size() / 2) return true;
+//     if(s[i] != s[s.size() - i -1]) return false;
+//     return f(i+1,s);
+// }
+
+
+int f(int n){
+    if(n <=1) return n;
+    int last = f(n-1);
+    int slast = f(n-2);
+    return last + slast;
 }
 
-int main () {
-
-    int num = 10;
-    doSomething(num);
-    cout << num <<endl;
+int main()
+{
+    // int n = 5;
+    // cout << fact(n);
+    // string s = "madsm";
+    // cout << f(0,s);
+    cout << f(5);
 
     return 0;
 }
---
+++ stllib.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print()
+{
+    cout << "raj";
+}
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    print();
+    int s = sum(5, 6);
+    cout << s;
+    return 0;
+}
+
+
+void explainPair() {
+    pair<int, int> 
+    
+}
